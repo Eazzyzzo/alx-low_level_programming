3298bfd69af2b79251729921a3ed38c67f1edcb1
Eazzyzzo <israeloguntolu@gmail.com>
2022-09-08 17:17:02 -0700

    puts printout

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this program diplays "Programming is like building a multilingual puzzle"
+ *
+ * Returns: 0 as success
+ */
+
+int main(void)
+
+{
+	/*string initialization*/
+
+	char put[] = "Programming is like building a multilingual puzzle";
+
+	/*writing 'put' to stdout */
+
+	puts(put);
+
+	return (0);
+
+}

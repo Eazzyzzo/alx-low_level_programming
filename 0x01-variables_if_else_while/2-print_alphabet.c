f2808ff23fc9a3ec904299189e353d6e88e0747f
Eazzyzzo <israeloguntolu@gmail.com>
2022-09-11 01:38:54 -0700

    number 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+**/
+*main - Entry point
+*
+*Return: 0 Always (success)
+*
+*/
+
+int main(void)
+
+{
+	char al;
+
+	for(al = 'a'; al <= z; al++;)
+
+	{
+		putchar(al);
+	}
+
+	{
+		putchar('\n');
+	}
+
+return (0);
+}

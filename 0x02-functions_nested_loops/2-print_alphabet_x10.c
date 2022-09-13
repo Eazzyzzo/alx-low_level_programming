6be34ed222d2dad2d4bffded106dc307a6bfb0a8
Eazzyzzo <israeloguntolu@gmail.com>
2022-09-13 15:07:01 -0700

    number2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,64 +1,37 @@
-
-ops
-Files
-TextThumbnail preview of a Drive item
-
-_putchar.c.txt
-TextThumbnail preview of a Drive item
-
-main.h.txt
-TextThumbnail preview of a Drive item
-
-Task 0.txt
-TextThumbnail preview of a Drive item
-
-Task 1.txt
-TextThumbnail preview of a Drive item
-
-Task 2.txt
-TextThumbnail preview of a Drive item
-
-Task 3.txt
-TextThumbnail preview of a Drive item
-
-Task 4.txt
-TextThumbnail preview of a Drive item
-
-Task 5.txt
-TextThumbnail preview of a Drive item
-
-Task 6.txt
-TextThumbnail preview of a Drive item
-
-Task 7.txt
-TextThumbnail preview of a Drive item
-
-Task 8.txt
-TextThumbnail preview of a Drive item
-
-Task 9.txt
-TextThumbnail preview of a Drive item
-
-Task 10.txt
-TextThumbnail preview of a Drive item
-
-Task 11.txt
-
 #include "main.h"
+
 /**
+ *
  *  * print_alphabet_x10 - function to print abc 10 times
+ *
  *   *
+ *
  *    * Return: 0
+ *
  *     */
+
 void print_alphabet_x10(void)
+
 {
+
 		char c, i;
 
+
+
 			for (i = 0; i <= 9; i++)
+
 					{
+
 								for (c = 'a'; c <= 'z'; c++)
+
 											{
+
 															_putchar(c);
+
 																	}
+
 										_putchar('\n');
+
 											}
+
+}

7f0be25e04313a1e69781f14065afb2238f7543e
Eazzyzzo <israeloguntolu@gmail.com>
2022-09-15 13:32:54 -0700

    number8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,57 +1,11 @@
-
-re f ... loops
-Files
-TextThumbnail preview of a Drive item
-
-main.h.txt
-TextThumbnail preview of a Drive item
-
-Task 0.txt
-TextThumbnail preview of a Drive item
-
-Task 1.txt
-TextThumbnail preview of a Drive item
-
-Task 2.txt
-TextThumbnail preview of a Drive item
-
-Task 3.txt
-TextThumbnail preview of a Drive item
-
-Task 4.txt
-TextThumbnail preview of a Drive item
-
-Task 5.txt
-TextThumbnail preview of a Drive item
-
-Task 6.txt
-TextThumbnail preview of a Drive item
-
-Task 7.txt
-TextThumbnail preview of a Drive item
-
-Task 8.txt
-TextThumbnail preview of a Drive item
-
-Task 9.txt
-TextThumbnail preview of a Drive item
-
-Task 10.txt
-TextThumbnail preview of a Drive item
-
-Task 11.txt
-TextThumbnail preview of a Drive item
-
-Task 12.txt
-
 #include <stdio.h>
 #include "main.h"
 
 /**
- * print_square - prints squares
- * @size: parameter
- * Return: returns nothing
- */
+ * *print_square - prints squares
+ * *@size: parameter
+ * *Return: returns nothing
+ * */
 
 void print_square(int size)
 {

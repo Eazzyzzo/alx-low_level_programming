03d831d4ea770819b8351aaf4e0b498df7c26f76
Eazzyzzo <israeloguntolu@gmail.com>
2022-10-25 14:30:32 -0700

    numer13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -10,24 +10,31 @@
  * @new: new node to add to the list
  * Return: pointer to the new list
  */
-
 listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+
 {
 	listint_t **newlist;
+
 	size_t i;
 
 	newlist = malloc(size * sizeof(listint_t *));
 
 	if (newlist == NULL)
+
 	{
 		free(list);
+
 		exit(98);
 	}
 
-	for (i = 0; i < ize - 1; i++)
+	for (i = 0; i < size - 1; i++)
+
 		newlist[i] = list[i];
+
 	newlist[i] = new;
+
 	free(list);
+
 	return (newlist);
 }
 
@@ -41,28 +48,43 @@ size_t free_listint_safe(listint_t **head)
 
 {
 	size_t i, num = 0;
+
 	listint_t **list = NULL;
+
 	listint_t *next;
 
 	if (head == NULL || *head == NULL)
+
 		return (num);
+
 	while (*head != NULL)
+
 	{
 		for (i = 0; i < num; i++)
 		{
 			if (*head == list[i])
+
 			{
 				*head = NULL;
+
 				free(list);
+
 				return (num);
 			}
 		}
+
 		num++;
+
 		list = _ra(list, num, *head);
+
 		next = (*head)->next;
+
 		free(*head);
+
 		*head = next;
 	}
+
 	free(list);
+
 	return (num);
 }

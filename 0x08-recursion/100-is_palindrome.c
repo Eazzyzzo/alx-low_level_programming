fc847d794f333c2258290e13be5c2ec5c3b09120
Eazzyzzo <israeloguntolu@gmail.com>
2023-06-07 23:35:11 -0700

    task 7 correction

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,38 +6,38 @@ int is_palindrome(char *s);
 
 /**
  * _strlen - Returns the length of a string.
- * @s: The string to be measured.
+ * @s: The string to be evaluated.
  * Return: The length of the string.
  */
 
 int _strlen(char *s)
 {
-	int sum = 0;
+	int len = 0;
 
-	if (*(s + sum))
+	if (*(s + len))
 	{
-		sum++;
-		sum = sum + _strlen(s + sum);
+		len++;
+		len = len + _strlen(s + len);
 	}
-	return (sum);
+	return (len);
 }
 
 /**
  * _palindrome - Checks if a string is a palindrome.
  * @s: The string to be evaluated.
- * @sum: The length of s.
- * @index: The index of the string to b evaluated.
+ * @len: The length of s.
+ * @index: The index of the string to be evaluated.
  * Return: If the string is a palindrome - 1.
  *         If the string is not a palindrome - 0.
  */
 
-int _palindrome(char *s, int sum, int index)
+int _palindrome(char *s, int len, int index)
 {
-	if (s[index] == s[sum / 2])
+	if (s[index] == s[len / 2])
 		return (1);
 
-	if (s[index] == s[sum - index - 1])
-		return (_palindrome(s, sum, index + 1));
+	if (s[index] == s[len - index - 1])
+		return (_palindrome(s, len, index + 1));
 
 	return (0);
 }
@@ -52,10 +52,10 @@ int _palindrome(char *s, int sum, int index)
 int is_palindrome(char *s)
 {
 	int index = 0;
-	int sum = _strlen(s);
+	int len = _strlen(s);
 
 	if (!(*s))
 		return (1);
 
-	return (check_palindrome(s, sum, index));
+	return (_palindrome(s, len, index));
 }

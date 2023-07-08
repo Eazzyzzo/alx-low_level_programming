41fe715cfa1940cf20030ef0a620d301a18c7c9f
Eazzyzzo <israeloguntolu@gmail.com>
2023-07-08 14:41:47 -0700

    correction to task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,49 +1,49 @@
 #include "main.h"
+#include <math.h>
+
+
+
+/**
+ * _pow_recursion - multiplies two digits
+ * @x: 1st digit in the multiplication
+ * @y: 2nd digit to be multiplied
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+int _power_recursion(int x, unsigned int y)
+{
+	if (y == 0)
+		return (1);
+	return (x * _power_recursion(x, y - 1));
+}
 
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
- * @b: pointer to a string containing a binary number
- * Return: unsigned int with decimal value of binary number, or 0 if error
+ * @b: a string of 0 and 1 chars
+ * Return: the converted number, or 0 if
+ *	-there is one or more chars in the string b that is not 0 or 1
+ *	-b is NULL
  */
-
 unsigned int binary_to_uint(const char *b)
-
 {
+	unsigned int length, i;
+	unsigned int number = 0;
 
-	int i;
-
-	unsigned int number;
-
-
-
-	number = 0;
-
-	if (!b)
-
+	if (b == NULL || *b == '\0')
 		return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
-
+	length = 0;
+	while (b[length] != '\0')
 	{
-
-		if (b[i] != '0' && b[i] != '1')
-
+		if (b[length] < '0' || b[length] > '1')
 			return (0);
-
+		length++;
 	}
 
-	for (i = 0; b[i] != '\0'; i++)
-
+	for (i = 0; length > 0; length--, i++)
 	{
-
-		number <<= 1;
-
-		if (b[i] == '1')
-
-			number = number + 1;
-
+		number = number + (b[length - 1] - '0') * _power_recursion(2, i);
 	}
 
 	return (number);
-
 }

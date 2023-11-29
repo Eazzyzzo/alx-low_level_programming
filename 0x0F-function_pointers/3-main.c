b83600164824e55f70263e379c7c738f6b978b41
root <you@example.com>
2023-11-29 15:12:31 -0800

    number 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,84 +2,28 @@
 
 /**
  * main - program that performs simple operations
- *
  * @argc: number of arguments
- *
  * @argv: array of arguments
- *
  * Return: 0 if successful
  */
 
 int main(int argc, char *argv[])
-
 {
+	int num1, num2, result;
 
-	int arg1, arg2, result;
-
-	char o;
-
-	int (*func)(int, int);
-
-
+	int (*funcptr)(int, int);
 
 	if (argc != 4)
-
 	{
-
-		printf("Error\n");
-
+		printf(" argument error \n");
 		exit(98);
-
 	}
 
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-
-	arg1 = atoi(argv[1]);
-
-	arg2 = atoi(argv[3]);
-
-
-
-	func = get_op_func(argv[2]);
-
-
-
-	if (!func)
-
-	{
-
-		printf("Error\n");
-
-		exit(99);
-
-	}
-
-
-
-	o = *argv[2];
-
-
-
-	if ((o == '/' || o == '%') && arg2 == 0)
-
-	{
-
-		printf("Error\n");
-
-		exit(100);
-
-	}
-
-
-
-	result = func(arg1, arg2);
-
-
-
-	printf("%d\n", result);
-
-
-
+	funcptr = get_op_func(argv[2]);
+	result = funcptr(num1, num2);
+	printf("result is; %i \n", result);
 	return (0);
-
 }
